2ea1d679abf94f3c1c500edbb340d5bf37a8f6dd
RosisK <rosiskharel5@gmail.com>
2025-01-17 19:41:28 +0545

    Add project files.

---
+++ OpenGL/snake.cpp
@@ -0,0 +1,29 @@
+//#define GLEW_STATIC
+//#include <GL/glew.h>
+//#include <GLFW/glfw3.h>
+//#include <iostream>
+//#include <vector>
+//#include <ctime>
+//#include <cstdlib>
+//
+//const int WINDOW_WIDTH = 800;
+//const int WINDOW_HEIGHT = 600;
+//const int GRID_SIZE = 20;
+//
+//struct Point
+//{
+//	int x, y;
+//};
+//
+//std::vector<Point> snake = { {10, 10} };
+//Point apple = { 15, 15 };
+//
+//bool gameOver = false;
+//
+//void drawSquare(float x, float y)
+//{
+//	glBegin(GL_QUADS);
+//	glVertex2f(x, y);
+//	glVertex2f();
+//	glEnd();
+//}
