a56a6c4413d2ec49df91617dd9e22c67861ae7e0
RosisK <rosiskharel5@gmail.com>
2025-01-19 19:52:00 +0545

    Added input control for arrow keys

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -99,15 +99,19 @@ void keyboardInput(GLFWwindow* window, int key, int scancode, int action, int mo
 		switch (key)
 		{
 		case GLFW_KEY_W:
+		case GLFW_KEY_UP:
 			if (direction.y == 0) direction = { 0, 1 };
 			break;
 		case GLFW_KEY_S:
+		case GLFW_KEY_DOWN:
 			if (direction.y == 0) direction = { 0, -1 };
 			break;
 		case GLFW_KEY_A:
+		case GLFW_KEY_LEFT:
 			if (direction.x == 0) direction = { -1, 0 };
 			break;
 		case GLFW_KEY_D:
+		case GLFW_KEY_RIGHT:
 			if (direction.x == 0) direction = { 1, 0 };
 			break;
 		}
